ad7071a73fedc348fd9f9c6a79afccea5afc3a7f
Vincent Zhang <seagle0128@gmail.com>
2017-10-02 12:58:52 +0800

    Update cask list.

--- install_brew_cask.sh
+++ install_brew_cask.sh
@@ -27,17 +27,19 @@ brew cask install emacs
 brew cask install clipy
 brew cask install cheatsheet
 brew cask install hyperswitch
-brew cask install scroll-reverser
+brew cask install scroll-reverser # Replaced by Mos
 brew cask install shadowsocksx-ng
 brew cask install fliqlo        # Screen Saver
 # brew cask install shiftit       # Replaced by spectacle
 brew cask install spectacle
-brew cask install sogouinput
+# brew cask install sogouinput
+brew cask install vanilla       # Hide menu bar icons
 
 brew cask install google-chrome
 brew cask install firefox
 # brew cask install karabiner-elements # karabiner: Keboard remapping
-brew cask install mplayerx
+brew cask install iina
+# brew cask install mplayerx
 brew cask install osx-fuse
 brew cask install veracrypt
 brew cask install soundflower   # Need soundflowerbed
