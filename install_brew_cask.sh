884eafe7b0c673011dd0fbe0c235dff3f65604f9
Vincent Zhang <seagle0128@gmail.com>
2017-10-18 21:54:44 +0800

    Update cask list.

--- install_brew_cask.sh
+++ install_brew_cask.sh
@@ -26,13 +26,13 @@ brew cask install cheatsheet
 brew cask install clipy
 brew cask install emacs
 brew cask install fliqlo        # Screen Saver
-brew cask install hyperswitch
+# brew cask install hyperswitch
 brew cask install iina          # Replace mplayerx
 brew cask install iterm2
 brew cask install keycastr      # Show keys on the screen
 brew cask install licecap       # Recording screen as gif
 brew cask install mounty          # Mounty for NTFS read/write
-brew cask install scroll-reverser # Alternative: MOS
+# brew cask install scroll-reverser # Alternative: MOS
 brew cask install shadowsocksx-ng
 brew cask install sound-siphon
 brew cask install soundflower
@@ -40,22 +40,23 @@ brew cask install soundflowerbed
 brew cask install spectacle
 # brew cask install vanilla       # Hide menu bar icons, buggy
 
-brew cask install firefox
+# brew cask install firefox
 brew cask install google-chrome
 # brew cask install karabiner-elements # karabiner: Keboard remapping
 # brew cask install netspot       # Wifi signal analysis and scanner
 brew cask install sourcetree
-brew cask install osx-fuse
-brew cask install veracrypt
+brew cask install osxfuse veracrypt
 brew cask install visual-studio-code
+brew cask install sogouinput
 
+# Optional
+# brew cask install acrobat-reader
 brew cask install aliwangwang
 brew cask install baidunetdisk
 # brew cask install bwana
 brew cask install java
 # brew cask install neteasemusic
 # brew cask install skype-for-business
-brew cask install sogouinput    # Replaced by builtin input
 brew cask install thunder
 
 # Cleanup
