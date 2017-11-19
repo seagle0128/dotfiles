0a81335e1f06bf309f2d16f69d3f8cb2d8f0f80d
Vincent Zhang <seagle0128@gmail.com>
2017-11-19 22:41:48 +0800

    Revert "Fix tput error."
    This reverts commit 73f73a19f18b81dd2675649e2a2625883836a389.

--- install.sh
+++ install.sh
@@ -12,14 +12,14 @@ sysOS=`uname -s`
 # supports them.
 if which tput >/dev/null 2>&1; then
     ncolors=$(tput colors)
-    if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-        RED="$(tput setaf 1)"
-        GREEN="$(tput setaf 2)"
-        YELLOW="$(tput setaf 3)"
-        BLUE="$(tput setaf 4)"
-        BOLD="$(tput bold)"
-        NORMAL="$(tput sgr0)"
-    fi
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
 else
     RED=""
     GREEN=""
