2b9679a2f1332d119676748ec56b5707319e9813
Vincent Zhang <seagle0128@gmail.com>
2017-11-20 22:28:09 +0800

    Add gotests.

--- install_go.sh
+++ install_go.sh
@@ -9,14 +9,18 @@ if not hash go 2>/dev/null; then
     echo "Error: go is not installed"
 fi
 
-go get -u github.com/nsf/gocode
-go get -u github.com/rogpeppe/godef
-go get -u github.com/golang/lint/golint
+# go tools
 go get -u golang.org/x/tools/cmd/godoc
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/gorename
 go get -u golang.org/x/tools/cmd/gotype
 go get -u golang.org/x/tools/cmd/guru
+
+# 3rd party
+go get -u github.com/nsf/gocode
+go get -u github.com/rogpeppe/godef
+go get -u github.com/golang/lint/golint
 go get -u github.com/derekparker/delve/cmd/dlv
 go get -u github.com/josharian/impl
+go get -u github.com/cweill/gotests/...
 go get -u sourcegraph.com/sqs/goreturns
