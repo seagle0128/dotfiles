13f0edc60cad1fabe9bbaf610c7aa8f0ea2c0ba8
Vincent Zhang <seagle0128@gmail.com>
2017-11-20 16:32:59 +0800

    Update go packages.

--- install_go.sh
+++ install_go.sh
@@ -12,10 +12,11 @@ fi
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
 go get -u github.com/golang/lint/golint
-go get -u golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/tools/cmd/guru
-go get -u golang.org/x/tools/cmd/gorename
 go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/gorename
+go get -u golang.org/x/tools/cmd/gotype
+go get -u golang.org/x/tools/cmd/guru
 go get -u github.com/derekparker/delve/cmd/dlv
 go get -u github.com/josharian/impl
 go get -u sourcegraph.com/sqs/goreturns
