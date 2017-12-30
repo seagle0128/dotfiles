96852c5e855fd5f6d1ddc25caa3317984708e3e4
Vincent Zhang <seagle0128@gmail.com>
2017-12-30 17:36:11 +0800

    Update cask list.

--- install_brew_cask.sh
+++ install_brew_cask.sh
@@ -24,10 +24,8 @@ fi
 # Cask
 brew cask install cheatsheet
 brew cask install clipy
-brew cask install emacs
 brew cask install fliqlo        # Screen Saver
 # brew cask install hyperswitch
-brew cask install iina          # Replace mplayerx
 brew cask install iterm2
 brew cask install keycastr      # Show keys on the screen
 brew cask install licecap       # Recording screen as gif
@@ -35,25 +33,29 @@ brew cask install mounty          # Mounty for NTFS read/write
 # brew cask install scroll-reverser # Alternative: MOS
 brew cask install shadowsocksx-ng
 # brew cask install sound-siphon
-brew cask install soundflower
-brew cask install soundflowerbed
+brew cask install soundflower soundflowerbed
 brew cask install spectacle     # window management
 # brew cask install vanilla       # Hide menu bar icons, buggy
 
-# brew cask install firefox
+brew cask install iina          # Replace mplayerx
+brew cask install firefox
 brew cask install google-chrome
 # brew cask install karabiner-elements # karabiner: Keboard remapping
 # brew cask install netspot       # Wifi signal analysis and scanner
-brew cask install sourcetree
 brew cask install osxfuse veracrypt
-brew cask install visual-studio-code
 brew cask install sogouinput
 
-# Optional
+# Development
+brew cask install emacs
+brew cask install docker
+# brew cask install java          # optional
+brew cask install sourcetree
+brew cask install visual-studio-code
+
+# Utils
 # brew cask install acrobat-reader
 brew cask install aliwangwang
 brew cask install baidunetdisk
-brew cask install java
 # brew cask install neteasemusic
 # brew cask install skype-for-business
 brew cask install thunder
