b70cef61b2cbf41c1dbba21b5dda439522e006dc
Vincent Zhang <seagle0128@gmail.com>
2018-08-21 16:46:12 +0800

    Install chocolatey packages on Windows.

---
+++ install_choco.ps1
@@ -0,0 +1,49 @@
+#############################################################
+# Install and update chocolatey packages on Windows
+# Author: Vincent Zhang <seagle0128@gmail.com>
+# URL: https://github.com/seagle0128/dotfiles
+#############################################################
+
+# Packages
+$packages = ("7zip", "clipx", "everything",
+             "emacs", "vscode",
+             "totalcommander", "wox",
+             "googlechrome", # "firefox",
+             "peco", "ag", "pt", "ripgrep",
+             # "golang", "python", "ruby", "nodejs"
+             # "jdk8", "jre8", "eclipse",
+             "sysinternals", "dependecywalker");
+
+function Test-Administrator
+{
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function check {
+    # check admin rights
+    if (-Not (Test-Administrator)) {
+        Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+        exit 1
+    }
+
+    # check if choco exists
+    if (-Not (Get-Command 'choco' -errorAction SilentlyContinue)) {
+        Write-Host "`n-> Installing Chocolatey..."
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+}
+
+function install {
+    foreach($p in $packages){
+        Write-Host "`n-> Installing $p..."
+        choco upgrade ${p} -y
+    }
+}
+
+function main {
+    check
+    install
+}
+
+main
