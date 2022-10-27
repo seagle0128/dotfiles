bd1f5dcc90ae92d4ad3f1a382c31609a66d38308
Vincent Zhang <seagle0128@gmail.com>
2022-10-27 10:50:54 +0800

    Add Emacs aliases.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,46 @@
 # Copy to $PROFILE
 
+Set-Variable DOTFILES "$env:APPDATA\.dotfiles"
+Set-Variable EMACSD "$env:APPDATA\.emacs.d"
+Set-Variable EDITOR 'emacsclientw -a ""'
+
+# Functions
+function Open-Mini-Emacs {
+    Invoke-Expression 'runemacs -Q -l "$EMACSD\init-mini.el"'
+}
+
+function Open-Mini-Terminal-Emacs {
+    Invoke-Expression 'emacs -Q -nw -l "$EMACSD\init-mini.el"'
+}
+
+function Open-Emacs-Client-Nowait {
+    param ([string]$FileName)
+    Invoke-Expression 'emacsclientw -a "" -n $FileName'
+}
+
+function Open-Emacs-Client-Frame-Nowait {
+    param ([string]$FileName)
+    Invoke-Expression 'emacsclientw -a "" -n -c $FileName'
+}
+
+function Open-Emacs-Client-Frame {
+    param ([string]$FileName)
+    Invoke-Expression 'emacsclientw -a "" -c $FileName'
+}
+
+function Open-Terminal-Emacs {
+    param ([string]$FileName)
+    Invoke-Expression 'emacs -Q -nw $FileName'
+}
+
+# Aliases
+Set-Alias -Name me  -Value Open-Mini-Emacs
+Set-Alias -Name mte -Value Open-Mini-Terminal-Emacs
+Set-Alias -Name e   -Value Open-Emacs-Client-Nowait
+Set-Alias -Name ec  -Value Open-Emacs-Client-Frame-Nowait
+Set-Alias -Name ef  -Value Open-Emacs-Client-Frame
+Set-Alias -Name te  -Value Open-Terminal-Emacs
+
 # Starship
 Invoke-Expression (&starship init powershell)
 
@@ -19,7 +60,7 @@ Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
 Set-PsFzfOption -TabExpansion
 
-# Aliases
+# Options
 Set-PsFzfOption -EnableAliasFuzzyEdit          # fe
 Set-PsFzfOption -EnableAliasFuzzyHistory       # fh
 Set-PsFzfOption -EnableAliasFuzzyKillProcess   # fkill
